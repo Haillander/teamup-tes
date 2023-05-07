d4480ce74c48d976919e07c598495ef04e00eefd
Haillander <haillanderviana@gmail.com>
2023-05-07 16:33:59 +1000

    trying to deploy on heroku

--- server/Utils/createEvent.js
+++
@@ -1,4 +0,0 @@
-const eventDate = document.getElementById("event-date");
-const eventTitle = document.getElementById("event-title");
-const eventDescription = document.getElementById("event-description");
-
